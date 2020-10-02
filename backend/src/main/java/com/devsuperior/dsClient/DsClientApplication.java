4cdbb7809bec70187385ffddb26621d3b58e764f
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-10-01 23:14:44 -0300

    Project Client initialized

---
+++ backend/src/main/java/com/devsuperior/dsClient/DsClientApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsClientApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/dsClient/DsClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
