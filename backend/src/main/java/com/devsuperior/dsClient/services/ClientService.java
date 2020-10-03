133f021f3c61a1644b7489641fbda191d267a3e5
Mauricio Quaranta <mauricioqvp@yahoo.com.br>
2020-10-02 23:19:42 -0300

    Delete functional

--- backend/src/main/java/com/devsuperior/dsClient/services/ClientService.java
+++ backend/src/main/java/com/devsuperior/dsClient/services/ClientService.java
@@ -6,12 +6,15 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsClient.dto.ClientDTO;
 import com.devsuperior.dsClient.entities.Client;
 import com.devsuperior.dsClient.repositories.ClientRepository;
+import com.devsuperior.dsClient.services.exceptions.DatabaseException;
 import com.devsuperior.dsClient.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -61,5 +64,15 @@ public class ClientService {
 		entity.setIncome(dto.getIncome());
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());		
+	}
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e){
+			throw new ResourceNotFoundException("Id not found " + id);
+		} catch (DataIntegrityViolationException e){
+			throw new DatabaseException("Integrity violation");
+		}
 	}
 }
